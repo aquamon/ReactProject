98cf07b8ebb5d145bd9893db8f53fc94bff1b2fd
Monash Chhetri <monash.chhetri22@gmail.com>
2023-06-23 19:51:57 +0530

    React_Fragment

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
 import React  from "react";
 import ReactDOM  from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//this tells where in HTML we want to run the react code.
 
-const heading1 = <h1 key="key1">This is the first heading</h1>;
+const Title = () => (
+    <a href="/">
+        <img className="logo" 
+            src = "https://img.freepik.com/premium-vector/good-food-logo-template_79169-17.jpg?w=740"
+                alt = "Logo"></img>
+    </a>
+);
 
 const HeaderComponent = () => {
     return (
-        <div>
-            {heading1}
-            <h2>This is the header HeaderComponent</h2>
-            <h2>This is the header HeaderComponent</h2>
+        <div className="header">
+            <Title/>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
+    )
+};
+
+const BodyComponent = () => {
+    return (
+        <h4>This is a Body</h4>
+    )
+};
+
+const FooterComponent = () => {
+    return (
+        <h4>This is a Footer</h4>
+    )
+};
+const AppLayout = () => {
+    return (
+        <React.Fragment>
+            <HeaderComponent/>
+            <BodyComponent/>
+            <FooterComponent/>
+        </React.Fragment>
     )
 };
 
 
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// console.log(HeaderComponent);
-// root.render(heading1);
-root.render(<HeaderComponent/>);
+root.render(<AppLayout/>);
 //passing a react element inside the root.
 //This render method, takes an object and modifies the DOM.
 //Hence DOM is rendered with the values of this react element, which is nothing but a 
