604ac4fd2b7653d56be4630ae82dadff2c91efcc
Monash Chhetri <monash.chhetri22@gmail.com>
2023-06-24 13:35:29 +0530

    Destructuring_the_object

--- app.js
+++ app.js
@@ -872,15 +872,15 @@ const restaurantList = [
       }
 ];
 
-const RestaurantCard = (props) => {
-    {console.log(props)}
+const RestaurantCard = ({restaurant}) => {
+    {console.log(restaurant)}
     return (
 
         <div className="card">
-            <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+props.restaurant.data?.cloudinaryImageId}></img>
-            <h2>{props.restaurant.data?.name}</h2>
-            <h3>{props.restaurant.data?.cuisines.join(', ')}</h3>
-            <h4>{props.restaurant.data?.avgRating}</h4>
+            <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+restaurant.data?.cloudinaryImageId}></img>
+            <h2>{restaurant.data?.name}</h2>
+            <h3>{restaurant.data?.cuisines.join(', ')}</h3>
+            <h4>{restaurant.data?.avgRating}</h4>
         </div>
     )
 }
