dab233ef15d527bcfcb6c4c89bea6b29c5d373e5
Monash Chhetri <monash.chhetri22@gmail.com>
2023-06-25 11:38:02 +0530

    useStateHook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
 
 const BodyComponent = () => {
+
+    const [searchInput , setSearchInput] = useState("Default value");
+
+    const [checkSearchClicked, setSearchClicked] = useState("false");
+
   return (
+     <>
+        <div className="search-container">
+            <input 
+                type="text"
+                className="search-input"
+                value={searchInput}
+                placeholder="Search"
+                onChange={(e) => {
+                    setSearchInput(e.target.value);
+                }}
+            ></input>
+            <button className="search-button" onClick={()=>{
+                if(checkSearchClicked == "false"){
+                    setSearchClicked("true");
+                }
+                else{
+                    setSearchClicked("false");
+                }
+
+            }}>Search</button>
+            <h1>{checkSearchClicked}</h1>
+        </div>
       <div className="restaurantList">
         {
           restaurantList.map((restaurant) => {
@@ -12,7 +40,8 @@ const BodyComponent = () => {
             )
           })
         }
-      </div>        
+      </div> 
+     </>       
   )
 };
 
