589497ac67e499f7d27ae25a9d4db061f7c7ad84
Monash Chhetri <monash.chhetri22@gmail.com>
2023-06-27 09:55:10 +0530

    ClassBasedComponent

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import ProfileClassComponent from "./ProfileClass";
 const AboutComponent = ()=>{
     return (
         <div>
             <h1>This is the About us page</h1>
             <Outlet/>
+            <ProfileClassComponent/>
 
         </div>
     )
---
+++ src/components/ProfileClass.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ProfileClassComponent extends React.Component{
+    render(){
+        return <h1>Profile from the Profile Class</h1>
+    }
+}
+
+export default ProfileClassComponent;
