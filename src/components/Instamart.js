ff4c174b9866e9852e73dcc9de5cf5657c75dde2
Monash Chhetri <monash.chhetri22@gmail.com>
2023-07-02 17:48:01 +0530

    Adding Sections in About Instamart

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,34 @@
 
+const Section = ({title,description})=>{
+    return(
+        <div className="p-2 m-2 border border-black">
+            <h3 className="font-bold text-xl">{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+
 
 const InstamartComponent = ()=>{
     return (
         <div>
-            <h1>This is the large Instamart Component</h1>
+            <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+            <Section
+                title={"About Instamart"}
+                description = {"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
+            />
+
+<Section
+                title={"Team Instamart"}
+                description = {"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
+            />
+
+<Section
+                title={"Careers Instamart"}
+                description = {"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
+            />
+
         </div>
     )
 }
